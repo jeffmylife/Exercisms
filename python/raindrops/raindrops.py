dcc3bc351454d9f28cdc912c8a5d68af90960325
jeffmylife <jeffmlife@gmail.com>
2020-04-05 16:25:34 -0700

    add raindrops

---
+++ python/raindrops/raindrops.py
@@ -0,0 +1,6 @@
+def convert(number):
+    ret = ""
+    if number%3==0: ret += "Pling"
+    if number%5==0: ret += "Plang"
+    if number%7==0: ret += "Plong"
+    return ret if len(ret)>0 else str(number)
