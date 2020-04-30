a4ddd01f2e586ad27fa9514cb80c6a6e49aa2f58
jeffmylife <jeffmlife@gmail.com>
2020-04-29 20:56:56 -0700

    improve raindrops.sh

--- bash/raindrops/raindrops.sh
+++ bash/raindrops/raindrops.sh
@@ -5,24 +5,24 @@ main () {
 
 	if (( $1 % 3 == 0 )) 
 	then
-		s="Pling"
+		s+="Pling"
 	fi
 
 	if (( $1 % 5 == 0 ))
         then
-                s="${s}Plang"
+                s+="Plang"
         fi
 
 	if (( $1 % 7 == 0 ))
         then
-                s="${s}Plong"
+                s+="Plong"
         fi
 
 	if [[ -z $s ]]
 	then
-		echo $1
+		echo "$1"
 	else 
-		echo $s
+		echo "$s"
 	fi
 
 }
